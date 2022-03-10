7494cf4adec49fd2ef8653bc4714990f297c7759
Nomesh88 <83343286+Nomesh88@users.noreply.github.com>
2022-03-10 12:28:25 +0530

    Update Naive_bayes.py

--- Naive_bayes.py
+++ Naive_bayes.py
@@ -1,5 +1,23 @@
 #Naive bayes
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import GaussianNB, MultinominalNB, BernouliNB
+import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+df=pd.read_csv('Data3.csv')
+sentences = df['word'].values
+y=df['label'].values
+#training and testing values
+sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.50, random_state=42)
+
+#converting the text data to numerical form
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train.astype('U'))
+X_train = vectorizer.transform(sentences_train.astype('U'))
+X_test  = vectorizer.transform(sentences_test.astype('U'))
+
+#loading the model file
 filename='Text model/finalized_model[naive-bayes].sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 loaded_model.fit(X_train.todense(),y_train)
