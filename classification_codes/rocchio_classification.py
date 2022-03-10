a7938bda15eef8da66da5bfa037b88e098b89e90
harsha3604 <66259880+harsha3604@users.noreply.github.com>
2022-03-10 14:15:18 +0530

    Add files via upload

--- classification_codes/rocchio_classification.py
+++ classification_codes/rocchio_classification.py
@@ -1,3 +1,6 @@
+#ROCCHIO CLASSIFICATION
+
+# IMPORTING THE LIBRARIES
 from sklearn.neighbors import NearestCentroid
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
@@ -6,9 +9,17 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.model_selection import train_test_split
+
+#DEFINING THE FEATURES AND TARGETS
+df=pd.read_csv('Data3.csv')
+sentences = df['word'].values
+y=df['label'].values
+
+#SPLITTING THE DATA FOR TRAINING AND TESTING USING TRAIN TEST SPLIT
 X, y = df.word.fillna(' '), df.label
 X_train, X_test, y_train,y_test = train_test_split(X,y, test_size = 0.2, random_state = 100) 
 
+#DEFINING A PIPELINE FOR THE MODEL
 text_clf = Pipeline([('vect', CountVectorizer()),
                      ('tfidf', TfidfTransformer()),
                      ('clf', NearestCentroid()),
@@ -16,7 +27,8 @@ text_clf = Pipeline([('vect', CountVectorizer()),
 
 text_clf.fit(X_train, y_train)
 
-
+#TESTING THE MODEL
 predicted = text_clf.predict(X_test)
 
+#PRINTING THE CLASSIFICAITON REPORT FOR THE MODEL
 print(metrics.classification_report(y_test, predicted))
--- classification_codes/svm.py
+++ classification_codes/svm.py
@@ -1,24 +1,32 @@
+#SUPPORT VECTOR MACHINES
+
+# IMPORTING THE LIBRARIES
 import pandas as pd
 import pickle
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import svm
 from sklearn.model_selection import train_test_split
+
+#DEFINING THE FEATURES AND TARGETS
 df=pd.read_csv('Data3.csv')
 sentences = df['word'].values
 y=df['label'].values
-#training and testing values
+
+#SPLITTING THE DATA FOR TRAINING AND TESTING USING TRAIN TEST SPLIT
 sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.50, random_state=42)
 
-#converting the text data to numerical form
+#CONVERT THE DATA INTO NUMERICAL FORM USING VECTORIZER
 vectorizer = CountVectorizer()
 vectorizer.fit(sentences_train.astype('U'))
 X_train = vectorizer.transform(sentences_train.astype('U'))
 X_test  = vectorizer.transform(sentences_test.astype('U'))
 
-#loading the model from file
+#LOADING THE MODEL FROM THE FILE
 filename='Text model/finalized_model[svm].sav'
 loaded_model = pickle.load(open(filename, 'rb'))
+
+#PREDICTION AND ACCURACY OF THE MODEL
 loaded_model.fit(X_train,y_train)
 result = loaded_model.score(X_test, y_test)
 print(result*100)
