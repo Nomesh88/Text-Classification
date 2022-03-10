1297109e7301d954fc25cd2293b02d575a4c0b14
Nomesh88 <83343286+Nomesh88@users.noreply.github.com>
2022-03-10 12:21:03 +0530

    Create Decision_tree.py

---
+++ Decision_tree.py
@@ -0,0 +1,24 @@
+#logistic regression
+import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+df=pd.read_csv('Data3.csv')
+sentences = df['word'].values
+y=df['label'].values
+#training and testing values
+sentences_train, sentences_test, y_train, y_test = train_test_split(sentences, y, test_size=0.50, random_state=42)
+
+#converting the text data to numerical form
+vectorizer = CountVectorizer()
+vectorizer.fit(sentences_train.astype('U'))
+X_train = vectorizer.transform(sentences_train.astype('U'))
+X_test  = vectorizer.transform(sentences_test.astype('U'))
+
+#decision tree Classifier
+from sklearn.tree import DecisionTreeClassifier
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X_train, y_train)
+result=dtree.score(X_test,y_test)
+print(result*100)
