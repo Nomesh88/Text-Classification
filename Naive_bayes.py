d8f4bdfe9e8bfa9cc689367ebf86826137b78c6f
Nomesh88 <83343286+Nomesh88@users.noreply.github.com>
2022-03-10 12:26:42 +0530

    Update Naive_bayes.py

--- Naive_bayes.py
+++ Naive_bayes.py
@@ -6,3 +6,21 @@ loaded_model.fit(X_train.todense(),y_train)
 result1=loaded_model.score(X_test.todense(),y_test)
 result = loaded_model.predict(X_test.todense())
 print(result1*100)
+
+
+#MultinominalNB Classifier
+filename='Text model/finalized_model[naive-bayes-multi].sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+loaded_model.fit(X_train.todense(),y_train)
+result1=loaded_model.score(X_test.todense(),y_test)
+result = loaded_model.predict(X_test.todense())
+print(result1*100)
+
+#BernouliNB() Classifier
+filename='Text model/finalized_model[naive-bayes-ber].sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+loaded_model.fit(X_train.todense(),y_train)
+result1=loaded_model.score(X_test.todense(),y_test)
+result = loaded_model.predict(X_test.todense())
+print(result1*100)
+
